d0c163923460d5fbfa5b2447630e58266d627fde
way322 <iliyamalihin@yandex.ru>
2026-04-16 18:55:37 +0300

    first commit2

--- database.js
+++ database.js
@@ -1,7 +1,7 @@
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
 
-const dbPath = path.resolve(__dirname, 'appointments.db');
+const dbPath = process.env.DB_PATH || path.resolve(__dirname, 'appointments.db');
 const db = new sqlite3.Database(dbPath);
 
 // Создание таблицы, если её нет
